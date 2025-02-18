b0850683b2c712ee5864bd584cc2a137f95f7676
rse0014 <sssrse.s0014@gmail.com>
2025-02-18 20:24:41 +0300

    Update sketch_Nikonov.ino

--- sketch_Nikonov/sketch_Nikonov.ino
+++ sketch_Nikonov/sketch_Nikonov.ino
@@ -2,12 +2,10 @@
 #ifdef __AVR__
 #include <avr/power.h>
 #endif
-int b;
-int a;
 int S=255;
 int H=255;
-int V=255
-int STR [64] ={
+int V=255;
+int STR[64] ={
     1,1,1,1,2,2,2,2,
     1,1,1,1,2,2,2,2,
     1,1,1,1,2,2,2,2,
@@ -24,96 +22,28 @@ Adafruit_NeoPixel pixels_2 = Adafruit_NeoPixel(64, 8, NEO_GRB + NEO_KHZ800);
 
 //Спосоности игроков
 //нажатие кнопки для начала игры
-void knop1(){
-  if (digital.read(4)==0){
+void debug(long a){
+    Serial.println(a);
+}
 
-    while (digital.read(4)==0){
+int k[8]={0,0,0,0,0,0,0,0};
+//------------------4-11------
+
+void knop(int a){
+  debug(digitalRead(a+3));
+  if (digitalRead(a+3)==0){
+    k[a]=1;
+    //debug(k[a]);
+    while (digitalRead(a+3)==0){
 
     }
     delay(50);
   }
 }
-void knop2(){
-  if (digital.read(5)==0){
 
-    while (digital.read(5)==0){
 
-    }
-    delay(50);
-  }
-}
-void knop1(){
-  if (digital.read(3)==0){
-
-    while (digital.read(3)==0){
-
-    }
-    delay(50);
-  }
-}
-void knop3(){
-  if (digital.read(2)==0){
-
-    while (digital.read(2)==0){
-
-    }
-    delay(50);
-  }
-}
-void knop4(){
-  if (digital.read(6)==0){
-
-    while (digital.read(6)==0){
-
-    }
-    delay(50);
-  }
-}
-void knop5(){
-  if (digital.read(8)==0){
-
-    while (digital.read(8)==0){
-
-    }
-    delay(50);
-  }
-}
-void knop6(){
-  if (digital.read(11)==0){
-
-    while (digital.read(11)==0){
-
-    }
-    delay(50);
-  }
-}
-void knop7(){
-  if (digital.read(10)==0){
-
-    while (digital.read(10)==0){
-
-    }
-    delay(50);
-  }
-}
-void knop8(){
-  if (digital.read(9)==0){
-
-    while (digital.read(9)==0){
-
-    }
-    delay(50);
-  }
-}
-нажатие кнопки игроком для повторения комбинации
-
-способности игры
-запомнить и вывести пиксель на матрицу
-запомнить и вывести комбинацию пикселей на матрицу
-издать звук
-
-void matrix(zasvet){
-  for (a=0;a>64;a++) {
+void matrix(int zasvet){
+    for (int a=0;a>64;a++) {
     if (zasvet == 1){
       if (STR[a] == 1){
 
@@ -138,11 +68,21 @@ void matrix(zasvet){
   }
 }
 void setup() {
+  Serial.begin(9600);
   // put your setup code here, to run once:
-
+  pinMode(4,INPUT_PULLUP);
+  pinMode(5,INPUT_PULLUP);
+  pinMode(6,INPUT_PULLUP);
+  pinMode(7,INPUT_PULLUP);
+  pinMode(8,INPUT_PULLUP);
+  pinMode(9,INPUT_PULLUP);
+  pinMode(10,INPUT_PULLUP);
+  pinMode(11,INPUT_PULLUP);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  knop(1);
+delay(100);
+ 
 }
